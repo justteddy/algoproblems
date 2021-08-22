c9a59eec554c44bbb3e0d28b974c417caf6835ef
justteddy <stas.didenko@aviasales.ru>
2021-08-22 15:47:18 +0700

    binary tree bfs description fixed

--- medium/binary_tree_bfs_traversal/main.go
+++ medium/binary_tree_bfs_traversal/main.go
@@ -5,7 +5,24 @@ import (
 )
 
 /*
- */
+	Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+	Example 1:
+	Input: root = [3,9,20,null,null,15,7]
+	Output: [[3],[9,20],[15,7]]
+
+	Example 2:
+	Input: root = [1]
+	Output: [[1]]
+
+	Example 3:
+	Input: root = []
+	Output: []
+
+	Constraints:
+	The number of nodes in the tree is in the range [0, 2000].
+	-1000 <= Node.val <= 1000
+*/
 func main() {
 	tree := &TreeNode{
 		Val: 3,
@@ -28,7 +45,6 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-// recursive solution
 func levelOrder(root *TreeNode) [][]int {
 	if root == nil {
 		return nil
