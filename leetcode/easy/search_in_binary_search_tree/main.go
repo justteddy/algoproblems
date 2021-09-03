5e06f1e59857bb1292243d78eb9f3103736e8cf0
justteddy <stas.didenko@aviasales.ru>
2021-09-03 18:28:54 +0700

    binary tree search

---
+++ leetcode/easy/search_in_binary_search_tree/main.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+/*
+	You are given the root of a binary search tree (BST) and an integer val.
+	Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+	Example 1:
+	Input: root = [4,2,7,1,3], val = 2
+	Output: [2,1,3]
+
+	Example 2:
+	Input: root = [4,2,7,1,3], val = 5
+	Output: []
+
+
+	Constraints:
+	The number of nodes in the tree is in the range [1, 5000].
+	1 <= Node.val <= 107
+	root is a binary search tree.
+	1 <= val <= 107
+*/
+
+func main() {
+	tree := &TreeNode{
+		Val:  1,
+		Left: nil,
+		Right: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val:   3,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: nil,
+		},
+	}
+
+	fmt.Println(searchBST(tree, 3))
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val == val {
+		return root
+	}
+
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	}
+
+	return searchBST(root.Right, val)
+}
