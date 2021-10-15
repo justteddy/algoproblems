542f816e71b1668249863c2baecbe1a5e0f23998
justteddy <rekzar7@gmail.com>
2021-10-15 15:12:28 +0700

    matrix 01

--- leetcode/medium/0_1_matrix/main.go
+++ leetcode/medium/0_1_matrix/main.go
@@ -11,57 +11,39 @@ func main() {
 }
 
 func updateMatrix(mat [][]int) [][]int {
-	seen := make(map[string]struct{})
-	var distanceToZero func(y, x int) int
-	distanceToZero = func(y, x int) int {
-		if x < 0 || x >= len(mat[0]) || y < 0 || y >= len(mat) {
-			return 10_000
-		}
-
-		if mat[y][x] == 0 {
-			return 0
-		}
-
-		if _, ok := seen[hash(y, x)]; ok {
-			return 1
-		}
-
-		seen[hash(y, x)] = struct{}{}
-		return 1 + min(
-			distanceToZero(y+1, x),
-			distanceToZero(y-1, x),
-			distanceToZero(y, x+1),
-			distanceToZero(y, x-1),
-		)
-	}
-
+	queue := make([][2]int, 0)
 	result := make([][]int, 0, len(mat))
 	for i, rows := range mat {
 		resultRow := make([]int, 0, len(mat[0]))
 		for j := range rows {
-			seen = make(map[string]struct{})
 			if mat[i][j] == 0 {
 				resultRow = append(resultRow, 0)
-				continue
+				queue = append(queue, [2]int{i, j})
+			} else {
+				resultRow = append(resultRow, 10_000)
 			}
-			resultRow = append(resultRow, distanceToZero(i, j))
 		}
 		result = append(result, resultRow)
 	}
 
-	return result
-}
+	dirs := [][2]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
+	for len(queue) != 0 {
+		curr := queue[0]
+		queue = queue[1:]
 
-func hash(y, x int) string {
-	return fmt.Sprintf("%d-%d", y, x)
-}
+		for _, dir := range dirs {
+			y, x := curr[0]+dir[0], curr[1]+dir[1]
+			if y < 0 || x < 0 || y >= len(result) || x >= len(result[0]) {
+				continue
+			}
 
-func min(vals ...int) int {
-	min := 10_000
-	for _, v := range vals {
-		if v < min {
-			min = v
+			if result[y][x] > result[curr[0]][curr[1]]+1 {
+				result[y][x] = result[curr[0]][curr[1]] + 1
+				queue = append(queue, [2]int{y, x})
+			}
 		}
+
 	}
-	return min
+
+	return result
 }
