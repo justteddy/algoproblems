b605476f3865c5aee107519e8b684e8629cd1bf3
justteddy <rekzar7@gmail.com>
2021-10-18 09:23:27 +0700

    triangle improve

--- leetcode/medium/triangle/main.go
+++ leetcode/medium/triangle/main.go
@@ -12,8 +12,19 @@ func main() {
 	}))
 }
 
-// recursion solution, too slow
+// bottom up
 func minimumTotal(triangle [][]int) int {
+	for i := len(triangle) - 2; i >= 0; i-- {
+		for j := range triangle[i] {
+			triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+		}
+	}
+
+	return triangle[0][0]
+}
+
+// recursion solution slow
+func minimumTotal_(triangle [][]int) int {
 	if len(triangle) == 1 {
 		return triangle[0][0]
 	}
