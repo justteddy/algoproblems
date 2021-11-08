c07e696f7834c887721b88fe31bd14057bb3527b
justteddy <rekzar7@gmail.com>
2021-11-08 12:15:11 +0700

    design hashset

--- leetcode/easy/design_hashset/main.go
+++ leetcode/easy/design_hashset/main.go
@@ -1,69 +1,32 @@
 package main
 
-import (
-	"container/list"
-)
-
 func main() {
 
 }
 
-const bucketSize = 1e05
+const bucketSize = 1e06 + 1
 
 type MyHashSet struct {
-	buckets [bucketSize]*list.List
+	buckets [bucketSize]*int
 }
 
 func Constructor() MyHashSet {
 	return MyHashSet{
-		buckets: [bucketSize]*list.List{},
+		buckets: [bucketSize]*int{},
 	}
 }
 
 func (h *MyHashSet) Add(key int) {
-	bucket := key % bucketSize
-	if h.buckets[bucket] == nil {
-		h.buckets[bucket] = list.New()
-		h.buckets[bucket].PushFront(key)
+	if h.buckets[key] == nil {
+		h.buckets[key] = &key
 		return
 	}
-
-	curr := h.buckets[bucket].Front()
-	for curr != nil {
-		if curr.Value.(int) == key {
-			return
-		}
-	}
-	h.buckets[bucket].PushFront(key)
 }
 
 func (h *MyHashSet) Remove(key int) {
-	bucket := key % bucketSize
-	if h.buckets[bucket] == nil {
-		return
-	}
-
-	curr := h.buckets[bucket].Front()
-	for curr != nil {
-		if curr.Value.(int) == key {
-			h.buckets[bucket].Remove(curr)
-			return
-		}
-	}
+	h.buckets[key] = nil
 }
 
 func (h *MyHashSet) Contains(key int) bool {
-	bucket := key % bucketSize
-	if h.buckets[bucket] == nil {
-		return false
-	}
-
-	curr := h.buckets[bucket].Front()
-	for curr != nil {
-		if curr.Value.(int) == key {
-			return true
-		}
-	}
-
-	return false
+	return h.buckets[key] != nil
 }
