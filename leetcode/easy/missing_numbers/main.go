f462abc7af2f4e43bed3d97fb4c7a0574b2461fd
justteddy <rekzar7@gmail.com>
2021-11-16 10:15:29 +0700

    missing numbers

--- leetcode/easy/missing_numbers/main.go
+++ leetcode/easy/missing_numbers/main.go
@@ -7,7 +7,23 @@ func main() {
 	fmt.Println(missingNumber([]int{9, 6, 4, 2, 3, 5, 7, 0, 1}))
 }
 
+// sum solution
 func missingNumber(nums []int) int {
+	expected := 0
+	for i := 0; i <= len(nums); i++ {
+		expected += i
+	}
+
+	actual := 0
+	for _, num := range nums {
+		actual += num
+	}
+
+	return expected - actual
+}
+
+// hash table solution
+func missingNumber_(nums []int) int {
 	dict := make(map[int]struct{})
 	for _, num := range nums {
 		dict[num] = struct{}{}
